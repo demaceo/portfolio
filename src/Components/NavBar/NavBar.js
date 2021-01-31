8a97dfdb9927c539266363dbc81cf02215a5c3bd
demaceo <hdemaceo@gmail.com>
2021-01-30 20:50:58 -0700

    Update NavBar icons

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -6,16 +6,16 @@ function NavBar() {
     <div className="nav-container">
       <ul className="nav-bar">
         <Link to="/" className="option">
-          <i className="fa fa-home" aria-hidden="true"></i>
+          <i className="fas fa-campground" aria-hidden="true"></i>
           <span className="option-text">Home</span>
         </Link>
         <Link to="/projects" className="option">
-          <i className="fa fa-plus" aria-hidden="true"></i>
+          <i className="fas fa-fingerprint" aria-hidden="true"></i>
           <span className="option-text">Projects</span>
         </Link>
         <Link to="/socials" className="option">
-          <i className="fa fa-th-large" aria-hidden="true"></i>
-          <span className="option-text">Socials</span>
+          <i class="fas fa-praying-hands" aria-hidden="true"></i>
+          <span className="option-text">Connect</span>
         </Link>
       </ul>
     </div>
