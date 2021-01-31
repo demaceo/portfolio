dcccf3d1c00904a115287e0c9efcd53dc0bd659d
demaceo <hdemaceo@gmail.com>
2021-01-30 22:20:20 -0700

    Add icons for bubbles

--- src/Components/Bubbles/Bubbles.js
+++ src/Components/Bubbles/Bubbles.js
@@ -6,17 +6,27 @@ export default function Bubbles() {
       <section className="bubble-container">
         <div className="bubble x1">
           <div className="text-container">
-            <h1 className="project-1">What's Cookin</h1>
+            <h1 className="project-1">
+              <i class="fas fa-cookie-bite icon" />
+              What's <br />Cookin
+            </h1>
           </div>
         </div>
         <div className="bubble x2">
           <div className="text-container">
-            <h1 className="project-2">Moody Tunes</h1>
+            <h1 className="project-2">
+              <i class="fas fa-music icon"/>
+              Moody Tunes
+            </h1>
           </div>
         </div>
         <div className="bubble x3">
           <div className="text-container">
-            <h1 className="project-3">Rancid Tomatillos</h1>
+            <h1 className="project-3">
+              <i class="fas fa-film icon"/>
+              Rancid <br />
+              Tomatillos
+            </h1>
           </div>
         </div>
       </section>
