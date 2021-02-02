f563a860c31448c41e675a9a0684d409dcc481f4
demaceo <hdemaceo@gmail.com>
2021-02-02 09:05:13 -0700

    Create a projectData file

---
+++ src/utilities/projectData.js
@@ -0,0 +1,39 @@
+import img1 from "./images/sample.jpg";
+import img2 from "./images/sample.jpg";
+import img3 from "./images/sample.jpg";
+
+const bio1 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus eros vitae sagittis interdum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas commodo, odio porta semper venenatis, justo mauris maximus dui, eget suscipit lectus turpis sed eros. Donec tincidunt laoreet porttitor. Donec hendrerit aliquam enim, nec viverra orci rutrum sed. Donec condimentum interdum velit quis fermentum. Ut placerat sapien eu malesuada pretium. Proin sollicitudin dictum mi.";
+
+const bio2 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus eros vitae sagittis interdum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas commodo, odio porta semper venenatis, justo mauris maximus dui, eget suscipit lectus turpis sed eros. Donec tincidunt laoreet porttitor. Donec hendrerit aliquam enim, nec viverra orci rutrum sed. Donec condimentum interdum velit quis fermentum. Ut placerat sapien eu malesuada pretium. Proin sollicitudin dictum mi.";
+
+const bio3 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus eros vitae sagittis interdum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas commodo, odio porta semper venenatis, justo mauris maximus dui, eget suscipit lectus turpis sed eros. Donec tincidunt laoreet porttitor. Donec hendrerit aliquam enim, nec viverra orci rutrum sed. Donec condimentum interdum velit quis fermentum. Ut placerat sapien eu malesuada pretium. Proin sollicitudin dictum mi.";
+
+
+const projectData = [
+  {
+    id: 1,
+    name: "What's Cookin",
+    image: img1,
+    description: bio1,
+    partners: ["Scott Brabson"],
+  },
+  {
+    id: 2,
+    name: "Moody Tunes",
+    image: img2,
+    description: bio2,
+    partners: ["Scott Brabson", "Bruce Gordon"],
+  },
+  {
+    id: 3,
+    name: "Rancid Tomatillos",
+    image: img3,
+    description: bio3,
+    partners: ["Caleb Cyphers"],
+  },
+];
+
+export default projectData;
