816f62d3eba22fbb3ea86d8037cbc3636bc14da3
demaceo <hdemaceo@gmail.com>
2021-02-02 13:43:05 -0700

    Add href tag to each project

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
 // import { useState, useEffect } from "react";
-import projects from "../../utilities/projectData.js";
+// import projects from "../../utilities/projectData.js";
 import NavBar from "../NavBar/NavBar";
 import Bubbles from "../Bubbles/Bubbles";
 import Header from "../Header/Header";
-
 // import Socials from "../Socials/Socials";
 // import LogIn from "../LogIn/LogIn";
 import Projects from "../Projects/Projects";
 // import AboutMe from "../AboutMe/AboutMe";
-
-
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import { useLocalStorage } from "../../utilities/useLocalStorage";
 
@@ -23,8 +19,7 @@ function App() {
         {/* <Route path="/login" render={() => <LogIn />} /> */}
         <Route exact path="/" render={() => <Bubbles />} />
         {/* <Socials /> */}
-        {/* <Route exact path="/project" render={() => <Project />} /> */}
-        {/* <Route exact path="/developer" render={() => <AboutMe />} /> */}
+        {/* <Route exact path="/aboutme" render={() => <AboutMe />} /> */}
         <Route exact path="/projects" render={() => <Projects />} />
         {/* <Route
           exact path="/projects"
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,16 +1,16 @@
 import React from "react";
 import projectData from "../../utilities/projectData.js";
 import './projects.css';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Projects() {
   const projects = projectData.map((project) => {
-    const { id, image, name, description, partners } = project;
+    const { id, image, name, description, partners, link } = project;
     return (
       <section className="project-container" id={id}>
-        <Link to={`projects/${name}/`}>
+        <a href={link}>
           <img className="project-img" src={image} key={id} id={id} />
-        </Link>
+        </a>
         <div className="project-details">
           <h1 id="project-title">{name}</h1>
           <p id="project-description">{description}</p>
