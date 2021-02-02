d1cdd409e23740dc9ab2b2a83bbb75859e0aea16
demaceo <hdemaceo@gmail.com>
2021-02-02 09:04:47 -0700

    Add a mobile-view for the Socials component

--- src/Components/Socials/Socials.js
+++ src/Components/Socials/Socials.js
@@ -19,5 +19,34 @@ export default function Socials() {
         <i id="mail" className="icon-envelope"></i>
       </div>
     </div>
+
+    /* 
+<body>
+  <h1>Mobile-View SOCIAL LINKS Buttons</h1>
+  <div class ="frame">
+    <a href="#" class="btn">
+ <i className="fab fa-facebook-f" style="color: #3b5998;"></i>
+</a>
+    <a href="#" class="btn">
+  <i className="fab fa-twitter" style="color: #00acee;"></i>
+</a>
+    <a href="#" class="btn">
+ <i className="fab fa-dribbble" style="color: #ea4c89;"></i>
+</a>
+    <a href="#" class="btn">
+ <i className="fab fa-linkedin-in" style="color:#0e76a8;"></i>
+</a>
+    <a href="#" class="btn">
+ <i className="fab fa-get-pocket" style="color:#ee4056;"></i>
+</a>
+    <a href="#" class="btn">
+ <i className="far fa-envelope"></i>
+</a>
+</div>
+</body>
+
+
+
+Resources1× 0.5× 0.25×Rerun*/
   );
 }
