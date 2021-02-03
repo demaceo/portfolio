72af252a737b4a0f4e9caddfeb0cf7bfb65f39f4
demaceo <hdemaceo@gmail.com>
2021-02-03 12:48:58 -0700

    Add Wave component

---
+++ src/Components/Waves/Waves.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './waves.css';
+
+export default function Waves() {
+    return (
+        <section id="container">
+      <div class="ocean">
+        <div class="wave"></div>
+        <div class="wave"></div>
+      </div>
+      </section>
+    );
+}
