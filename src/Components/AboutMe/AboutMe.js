f65351eb9ddeadda7132779a9158dcb96ab6c857
demaceo <hdemaceo@gmail.com>
2021-02-02 22:33:49 -0700

    Add socials and details to AboutMe component

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./aboutMe.css";
 
 export default function AboutMe() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="about">
+      <h2 id="my-name">Demaceo Vincent Howard</h2>
+      <h4 id="title">Front End Engineer // Software Developer</h4>
+      <div id="socials">
+        <a
+          className="social fab fa-linkedin"
+          href="https://www.linkedin.com/in/demaceo-vincent-howard-12a9028b/"
+        />
+        <a
+          className="social fab fa-instagram"
+          href="https://www.instagram.com/demaceo.vh/"
+        />
+        <a className="social fab fa-github" href="https://github.com/demaceo" />
+
+        <a className="social far fa-file" />
+      </div>
+    </div>
+  );
 }
