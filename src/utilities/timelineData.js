e16edc72fa690c91add61587d6ab51d3526cc368
demaceo <hdemaceo@gmail.com>
2021-02-02 22:39:57 -0700

    Create timelineDate file

---
+++ src/utilities/timelineData.js
@@ -0,0 +1,52 @@
+const timelineData = [
+  {
+    id: 1,
+    job: "",
+    location: "The Americas",
+    text: "Started working on the app-ideas repository",
+    date: "June 2018",
+    image: "",
+    category: {
+      tag: "Environmental Educator",
+      color: "#FFDB14",
+    },
+  },
+  {
+    id: 2,
+    job: "",
+    location: "Andros, Bahamas",
+    text: "Started working on the app-ideas repository",
+    date: "June 2017",
+    image: "",
+    category: {
+      tag: "Marine Research",
+      color: "#e17b77",
+    },
+  },
+  {
+    id: 3,
+    job: "",
+    location: "Utila, Honduras",
+    text: "Spent a summer on an island learning how to become a Dive Master",
+    date: "June 2016",
+    image: "",
+    category: {
+      tag: "Scuba Dive",
+      color: "#1DA1F2",
+    },
+  },
+  {
+    id: 4,
+    job: "",
+    location: "Osa Peninsula, Costa Rica",
+    text: "Lived in the rainforest doing wildlife research conservation",
+    date: "Oct 2014",
+    image: "",
+    category: {
+      tag: "Field Research",
+      color: "#018f69",
+    },
+  },
+];
+
+export default timelineData;
