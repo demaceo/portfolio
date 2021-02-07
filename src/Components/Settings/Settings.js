4ff55e3608b9a31bb850ef777994fabfd2b79a6a
demaceo <hdemaceo@gmail.com>
2021-02-07 10:56:52 -0700

    Add Settings component

---
+++ src/Components/Settings/Settings.js
@@ -0,0 +1,35 @@
+import "./settings.css";
+import React, { Component } from "react";
+import { useRef, useEffect } from "react";
+import LightSwitch from "../LightSwitch/LightSwitch";
+
+function Settings() {
+  const switchRef = useRef(0);
+  const navRef = useRef();
+  
+  const toggleSettings = () => {
+    if (!switchRef.current) {
+      navRef.current.classList.remove("hidden");
+      switchRef.current = 1;
+    } else {
+      navRef.current.classList.add("hidden");
+      switchRef.current = 0;
+    }
+  };
+  return (
+    <>
+      <i className="fas fa-cog" id="settings-icon" onClick={toggleSettings}></i>
+      <div className="sidenav hidden" ref={navRef}>
+        <a href="/aboutme">About Me</a>
+        <a href="/recommendations">Recommendations</a>
+        <a href="/gazer">Gazer</a>
+        <a href="/map">Map</a>
+        <br />
+        <a href="/login">Login</a>
+        <LightSwitch />
+      </div>
+    </>
+  );
+}
+
+export default Settings;
