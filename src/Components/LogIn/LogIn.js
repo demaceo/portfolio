c44fbf90d7b417dcab0f446ace5804e9460b12bb
demaceo <hdemaceo@gmail.com>
2021-02-07 10:57:15 -0700

    Adjust stylings to Login

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -6,9 +6,6 @@ export default function LogIn() {
   //     e.preventDefault()
   //};
   //<i class="far fa-sticky-note"></i>
-  //<i class="fas fa-cog"></i>
-  //<i class="fas fa-toggle-on"></i>
-  //<i class="fas fa-toggle-off"></i>
 
   return (
     <div className="full-screen-container">
@@ -16,24 +13,26 @@ export default function LogIn() {
         <h3 className="login-title">Welcome</h3>
         <form>
           <div className="input-group">
-            <label>Email</label>
-            <input type="email" />
+            <label>Name</label>
+            <input type="text" />
           </div>
           <div className="input-group">
             <label>Password</label>
             <input type="password" />
           </div>
-          {/* <button type="submit" className="login-button">Sign In</button> */}
-          <button>
+          {/* <button type="submit" className="login-button">
+            Sign In
+          </button> */}
+          <button type="submit">
             <span>Sign In</span>
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
               viewBox="0 0 24 24"
             >
               <path d="M0 11c2.761.575 6.312 1.688 9 3.438 3.157-4.23 8.288-8.187 15-11.438-5.861 5.775-10.711 12.328-14 18.917-2.651-3.766-5.547-7.271-10-10.917z" />
-            </svg>
+            </svg> */}
           </button>
         </form>
       </div>
