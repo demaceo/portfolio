a0c84026d2a0671be4c98ab50ba68ad9ebd71535
demaceo <hdemaceo@gmail.com>
2021-02-11 16:57:51 -0700

    Adjust positioning of information

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -4,8 +4,10 @@ import "./aboutMe.css";
 export default function AboutMe() {
   return (
     <div className="about">
-      <h2 id="my-name">Demaceo Vincent Howard</h2>
-      <h4 id="title">Front End Engineer // Software Developer</h4>
+      <div>
+        <h2 id="my-name">Demaceo Vincent Howard</h2>
+        <h4 id="title">Front End Engineer // Software Developer</h4>
+      </div>
       <div id="socials">
         <a
           href="https://www.linkedin.com/in/demaceo-vincent-howard-12a9028b/"
@@ -20,7 +22,6 @@ export default function AboutMe() {
         <a href="https://github.com/demaceo" alt="github">
           <i className="social fab fa-github"></i>
         </a>
-
         <a
           href="https://alumni.turing.io/sites/default/files/resumes/FE%202021%20Resume.pdf"
           // href="https://alumni.turing.io/alumni/demaceo-vincent"
