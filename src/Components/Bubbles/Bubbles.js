ae43ce0d3890e2a665b08b88a0ba152936e26721
demaceo <hdemaceo@gmail.com>
2021-02-12 11:36:18 -0700

    Create and add functionality to getStyle

--- src/Components/Bubbles/Bubbles.js
+++ src/Components/Bubbles/Bubbles.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import './bubbles.css';
+import React from "react";
+import "./bubbles.css";
 import projectData from "../../utilities/projectData.js";
-// import { Link } from "react-router-dom";
 
 export default function Bubbles() {
+  const getStyle = (event) => {
+    event.target.classList.add("pop");
+    setTimeout(() => {
+      window.open(event.target.id);
+      event.target.classList.remove("pop");
+    }, 1000);
+  };
   const bubbles = projectData.map((bubble) => {
-    const { id, name, icon } = bubble;
+    const { id, name, icon, link } = bubble;
     return (
-      <div className={`bubble x${id}`} key={id} id={id}>
+      <div className={`bubble x${id}`} key={id} id={link} onClick={getStyle}>
         <div className={`text-container project-${id}`}>
           <h1>{name}</h1>
           <i className={`${icon} icon`} />
         </div>
       </div>
     );
-})
+  });
   return <section className="bubble-container">{bubbles}</section>;
 }
