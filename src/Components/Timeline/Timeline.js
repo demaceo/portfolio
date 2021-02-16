32ba0377dd4fd81e40c734da528a7d35b7697d73
demaceo <hdemaceo@gmail.com>
2021-02-16 12:34:22 -0700

    Map through descriptors

--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./timeline.css";
 import timelineData from "../../utilities/timelineData.js";
+import { nanoid } from "nanoid";
 
 export default function Timeline() {
   const timelineItems = timelineData.map((item) => {
-    const { id, location, category, text, date } = item;
+    const { id, location, category, description, date } = item;
     return (
       <section className="timeline-item" key={id} id={id}>
         <div className="timeline-item-content">
@@ -15,8 +16,15 @@ export default function Timeline() {
           </span>
           <h1 className="job-location">{location}</h1>
           {/* <h2 className="job-title">{job}</h2> */}
-          {/* <p className="job-info" contentEditable="true">{text}</p> */}
-          <p className="job-info">{text}</p>
+          <ul className="job-info">
+          {description.map((descriptor) => {
+            return (
+              <li className="info-text" key={nanoid()}>
+                {descriptor.point}
+              </li>
+            );
+          })}
+          </ul>
         </div>
       </section>
     );
