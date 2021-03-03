e6bacb4da7df5a8b4b91b7e7e46c443f6776f4f0
demaceo <hdemaceo@gmail.com>
2021-03-03 12:23:11 -0700

    Import ThemeContext to LightSwitch

--- src/Components/LightSwitch/LightSwitch.js
+++ src/Components/LightSwitch/LightSwitch.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { useRef } from "react";
 import "./lightSwitch.css";
+import { useTheme, useThemeUpdate } from "../../utilities/ThemeContext";
 
 export default function LightSwitch() {
   const clickRef = useRef(0);
   const offRef = useRef();
   const onRef = useRef();
   const switchRef = useRef();
+  const dark = useTheme();
+  const themeStyles = {
+    backgroundColor: dark ? "#333" : "#CCC",
+    color: dark ? "#333" : "#CCC",
+    padding: "2rem",
+    margin: "2rem",
+  };
+  const toggleTheme = useThemeUpdate();
 
   const adjustLight = () => {
-    // let body = document.body;
     if (clickRef.current) {
       onRef.current.classList.add("hidden");
       offRef.current.classList.remove("hidden");
       clickRef.current = 0;
     } else {
-      // document.body.style.backgroundColor = "white";
       onRef.current.classList.remove("hidden");
       offRef.current.classList.add("hidden");
       clickRef.current = 1;
@@ -23,9 +30,15 @@ export default function LightSwitch() {
   };
 
   return (
-    <form className="light-switch" ref={switchRef} onClick={adjustLight}>
+    <button
+      className="light-switch"
+      onClick={toggleTheme}
+      ref={switchRef}
+      // onClick={adjustLight}
+      
+    >
       <i className="far fa-moon switch" ref={onRef}></i>
       <i className="fas fa-moon switch hidden" id="full" ref={offRef}></i>
-    </form>
+    </button>
   );
 }
