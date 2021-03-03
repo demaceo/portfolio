70a3f98f4ff060d87daf238959617400344c212e
demaceo <hdemaceo@gmail.com>
2021-03-03 10:23:06 -0700

    Create Matrix button

---
+++ src/Components/Matrix/Matrix.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Matrix.css';
+
+export default function Matrix() {
+    return (
+        <div id="matrix-container">
+            <div id="matrix-pill">||</div>
+        </div>
+    )
+}
