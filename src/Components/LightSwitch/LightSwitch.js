3ffa1f8f83f8d377d0880a9a26950d2152e16fcf
demaceo <hdemaceo@gmail.com>
2021-03-03 14:46:16 -0700

    Remove Refs and adjustLight function

--- src/Components/LightSwitch/LightSwitch.js
+++ src/Components/LightSwitch/LightSwitch.js
@@ -4,41 +4,13 @@ import "./lightSwitch.css";
 import { useTheme, useThemeUpdate } from "../../utilities/ThemeContext";
 
 export default function LightSwitch() {
-  const clickRef = useRef(0);
-  const offRef = useRef();
-  const onRef = useRef();
-  const switchRef = useRef();
-  const dark = useTheme();
-  const themeStyles = {
-    backgroundColor: dark ? "#333" : "#CCC",
-    color: dark ? "#333" : "#CCC",
-    padding: "2rem",
-    margin: "2rem",
-  };
+  const darkTheme = useTheme();
   const toggleTheme = useThemeUpdate();
-
-  const adjustLight = () => {
-    if (clickRef.current) {
-      onRef.current.classList.add("hidden");
-      offRef.current.classList.remove("hidden");
-      clickRef.current = 0;
-    } else {
-      onRef.current.classList.remove("hidden");
-      offRef.current.classList.add("hidden");
-      clickRef.current = 1;
-    }
-  };
+  const moonStyle = darkTheme ? "fas fa-moon switch" : "far fa-moon switch";
 
   return (
-    <button
-      className="light-switch"
-      onClick={toggleTheme}
-      ref={switchRef}
-      // onClick={adjustLight}
-      
-    >
-      <i className="far fa-moon switch" ref={onRef}></i>
-      <i className="fas fa-moon switch hidden" id="full" ref={offRef}></i>
-    </button>
+    <form className="light-switch" onClick={toggleTheme}>
+      <i className={moonStyle}></i>
+    </form>
   );
 }
