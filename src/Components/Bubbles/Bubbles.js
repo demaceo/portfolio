a19a00b2b55ed4dedac04f67a87e9b8d4f66c3cb
demaceo <hdemaceo@gmail.com>
2021-04-06 11:53:49 -0600

    Add conditional to limit Bubbles to 3 projects

--- src/Components/Bubbles/Bubbles.js
+++ src/Components/Bubbles/Bubbles.js
@@ -12,15 +12,17 @@ export default function Bubbles() {
   };
   const bubbles = projectData.map((bubble) => {
     const { id, name, icon, link } = bubble;
-    return (
-      <div className={`bubble x${id}`} key={id} id={link} onClick={getStyle}>
-        <div className={`text-container project-${id}`}>
-          {/* put 'text-container' in h1 element */}
-          <h1>{name}</h1>
-          <i className={`${icon} icon`} />
+    if (id < 4) {
+      return (
+        <div className={`bubble x${id}`} key={id} id={link} onClick={getStyle}>
+          <div className={`text-container project-${id}`}>
+            {/* put 'text-container' in h1 element */}
+            <h1>{name}</h1>
+            <i className={`${icon} icon`} />
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   });
   return <section className="bubble-container">{bubbles}</section>;
 }
