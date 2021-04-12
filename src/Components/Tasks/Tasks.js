709cfde0fc1215780e19ea0dda87072812a6f546
demaceo <hdemaceo@gmail.com>
2021-04-12 12:19:17 -0600

    Clear Task input value on Enter

--- src/Components/Tasks/Tasks.js
+++ src/Components/Tasks/Tasks.js
@@ -4,7 +4,6 @@ import "./Tasks.css";
 class Tasks extends Component {
   constructor() {
     super();
-    // state variables
     this.state = {
       tasks: [],
       value: "",
@@ -16,17 +15,13 @@ class Tasks extends Component {
     this.removeTask = this.removeTask.bind(this);
     this.updateHelp = this.updateHelp.bind(this);
 
-    // Query Selector variables
-    // this.root = document.querySelector("#taskList");
     this.form = document.querySelector("#taskForm");
     this.input = document.querySelector("#taskInput");
     this.help = document.querySelector("#taskHelp");
     this.ul = document.querySelector("#taskul");
-    this.items = {}; // id -> li element
+    this.items = {}; 
   }
 
-  // Event Handlers
-  // if (document.body.contains(document.getElementById("taskForm"))) {
   handleChange = (e) => {
     this.setState({ value: e.target.value });
   };
@@ -35,46 +30,42 @@ class Tasks extends Component {
     e.preventDefault();
     const task = this.state.value;
     this.addTask(task);
-    // this.input.value = "";
+    const taskText = document.querySelector("#taskInput");
+    taskText.value = "";
   };
 
   handleDelete = (e) => {
     e.preventDefault();
     const id = e.target.getAttribute("data-delete-id");
-    if (!id) return; // user clicked on something else
+    if (!id) return;
     this.removeTask(id);
   };
 
   addTask = (task) => {
-    // state logic
     const id = String(Date.now());
     this.state.tasks = this.state.tasks.concat({ task, id });
-    // UI logic
     this.updateHelp();
     const ul = document.querySelector("#taskul");
     const li = document.createElement("li");
     const span = document.createElement("span");
     const del = document.createElement("a");
     span.innerText = task;
-    del.innerText = "delete";
+    del.innerText = "X";
     del.setAttribute("data-delete-id", id);
     ul.appendChild(li);
-    li.appendChild(del);
     li.appendChild(span);
+    li.appendChild(del);
     this.items[id] = li;
   };
 
   removeTask = (id) => {
     const ul = document.querySelector("#taskul");
-    // state logic
     this.state.tasks = this.state.tasks.filter((item) => item.id !== id);
-    // UI logic
     this.updateHelp();
     const li = this.items[id];
     ul.removeChild(li);
   };
 
-  // utility method
   updateHelp = () => {
     if (this.state.tasks.length > 0) {
       document.querySelector(".help").classList.add("hidden");
