8d7e0fd60e2866f949dc8fe06e55721b34d2de87
demaceo <hdemaceo@gmail.com>
2023-03-13 13:28:24 -0600

    update moody tunes link

--- src/utilities/projectData.js
+++ src/utilities/projectData.js
@@ -11,7 +11,7 @@ const projectData = [
       { name: "Scott Brabson", link: "https://github.com/brabbuss" },
       { name: "Bruce Gordon", link: "https://github.com/bruce-gordon" },
     ],
-    link: "https://brabbuss.github.io/moodytunes/",
+    link: "https://github.com/demaceo/moodytunes",
     icon: "fas fa-theater-masks icon",
   },
   {
@@ -28,7 +28,7 @@ const projectData = [
       { name: "Sage Gonzales", link: "https://github.com/SageOfCode" },
       { name: "Brian Liu", link: "https://github.com/the-color-bliu" },
     ],
-    link: "https://cappua-fe.herokuapp.com/",
+    link: "https://github.com/Cappua/cappua-fe",
     icon: "fas fa-music icon",
   },
   {
