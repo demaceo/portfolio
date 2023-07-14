b4632ad6d5d9b862817d2d22b92758822a2e63d4
demaceo <hdemaceo@gmail.com>
2023-07-13 20:02:25 -0600

    add resume link

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -41,13 +41,9 @@ export default function AboutMe() {
             <i className="social fab fa-github"></i>
           </a>
           <a
-            href="https://alumni.turing.io/sites/default/files/resumes/FE%202021%20Resume.pdf"
-            // href="https://alumni.turing.io/alumni/demaceo-vincent"
+            href="https://turing-terminal-production.s3.us-east-2.amazonaws.com/lw1r9fg9qwabdksuwjkj069zy508?response-content-disposition=inline%3B%20filename%3D%22Resume_Demaceo_2023.pdf%22%3B%20filename%2A%3DUTF-8%27%27Resume_Demaceo_2023.pdf&response-content-type=application%2Fpdf&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3LJHOIGWOPTBZWP6%2F20230712%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20230712T185319Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=7f9dcf0a0e5efc79f422e3ce929948127303fc05350025fbc6d6e4ce41ce5c99"
             alt="demaceos-resume"
-            // href="demaceos.pdf"
-            // download
           >
-            {/* <img className="social" src="https://img.icons8.com/carbon-copy/100/000000/resume.png" /> */}
             <i className="far fa-file-alt" />
           </a>
         </div>
