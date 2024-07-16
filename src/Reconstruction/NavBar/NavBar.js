ce5a33b70338904688d74fdb2a6896754fe5e3fa
demaceo <hdemaceo@gmail.com>
2024-07-16 16:42:42 -0600

    reconstruct NavBar

--- src/Reconstruction/NavBar/NavBar.js
+++ src/Reconstruction/NavBar/NavBar.js
@@ -1,23 +1,28 @@
 import "./NavBar.css";
 import { Link } from "react-router-dom";
+import logo from "../../logo.jpg";
 
 function NavBar() {
   return (
-    <div className="nav-container">
-      <div className="nav-logo">
-        
+    <div className="nav-container syne-font">
+      <div className="logo-container">
+        <img id="logo" src={logo} />
+        <div className="descriptor-container">
+          <div id="name">Demaceo Vincent</div>
+          <div id="occupation">Designer and Developer</div>
+        </div>
       </div>
       <div className="nav-links">
-        <Link to="/" className="option">
-          <i className="fa fa-campground" aria-hidden="true"></i>
+        <Link to="/" className="link">
+          <i className="fa fa-id-badge" aria-hidden="true"></i>
           <span className="option-text">Profile</span>
         </Link>
-        <Link to="/projects" className="option">
-          <i className="fa fa-stream" aria-hidden="true"></i>
+        <Link to="/portfolio" className="link">
+          <i className="fa fa-layer-group" aria-hidden="true"></i>
           <span className="option-text">Portfolio</span>
         </Link>
-        <Link to="/experience" className="option">
-          <i className="fas fa-fingerprint fa" aria-hidden="true"></i>
+        <Link to="/contact" className="link">
+          <i className="fa fa-paper-plane" aria-hidden="true"></i>
           <span className="option-text">Contact</span>
         </Link>
       </div>
