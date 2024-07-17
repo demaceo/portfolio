1291c7b4e0d8ba8ca7e7b94867ce96f28dff84cd
demaceo <hdemaceo@gmail.com>
2024-07-16 18:36:56 -0600

    build out Hero component

--- src/Reconstruction/Hero/Hero.js
+++ src/Reconstruction/Hero/Hero.js
@@ -1,1 +1,12 @@
 import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <section className="hero-container">
+      <div className="hero-text-container">
+        <h1 id="top-text">Making the invisible</h1>
+        <h1 id="bttm-text">VISIBLE</h1>
+      </div>
+    </section>
+  );
+}
