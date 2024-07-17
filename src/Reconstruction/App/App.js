0f0b006443f36b0f66c9583dd9d7472649b36c7f
demaceo <hdemaceo@gmail.com>
2024-07-17 15:45:31 -0600

    add Portfolio component

--- src/Reconstruction/App/App.js
+++ src/Reconstruction/App/App.js
@@ -1,43 +1,40 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-// import Layout from "../Layout/Layout.js";
-
-// import Projects from "../Projects/Projects.js";
 import NavBar from "../../Reconstruction/NavBar/NavBar.js";
-import Hero from "../../Reconstruction/Hero/Hero.js"
+import Hero from "../../Reconstruction/Hero/Hero.js";
 import Profile from "../Profile/Profile.js";
 import Contact from "../Contact/Contact.js";
-
+import Portfolio from "../Portfolio/Portfolio.js"
 
 function App() {
   return (
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={
-            <>
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={
+          <>
             <NavBar />
-            <Hero/>
-            <Profile/>
-            <Contact/>
-            </>
-          }
-        />
-        {/* <Route exact path="/projects" element={<Projects />} /> */}
-        <Route
-          exact
-          path="/experience"
-          element={
-            <>
-              {/* <AboutMe /> */}
-              {/* <Timeline /> */}
-            </>
-          }
-        />
-        {/* <Route exact path="/pg" element={<AllLinksPage />} /> */}
-      </Routes>
-  )
+            <Hero />
+            <Profile />
+            <Contact />
+          </>
+        }
+      />
+      <Route
+        exact
+        path="/portfolio"
+        element={
+          <>
+            <NavBar />
+            <Portfolio/>
+            <Contact />
+          </>
+        }
+      />
+      {/* <Route exact path="/pg" element={<AllLinksPage />} /> */}
+    </Routes>
+  );
 }
 
 export default App;
