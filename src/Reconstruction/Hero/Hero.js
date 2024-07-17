3096d38d21ef0c64efca557e7750bdcbafaab819
demaceo <hdemaceo@gmail.com>
2024-07-17 13:51:35 -0600

    add hover state to Hero text

--- src/Reconstruction/Hero/Hero.js
+++ src/Reconstruction/Hero/Hero.js
@@ -1,9 +1,15 @@
 import "./Hero.css";
+import React, { useState } from "react";
 
 export default function Hero() {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <section className="hero-container">
-      <div className="hero-text-container">
+      <div
+        className={`hero-text-container ${hovered ? "hovered" : ""}`}
+        onMouseEnter={() => setHovered(true)}
+      >
         <h1 id="top-text">Making the invisible</h1>
         <h1 id="bttm-text">VISIBLE</h1>
       </div>
