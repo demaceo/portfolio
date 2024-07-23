0a3e7dc67f67cbd8a5a7dc22d98571416d4c41c8
demaceo <hdemaceo@gmail.com>
2024-07-23 13:33:18 -0600

    add required input fields

--- src/Reconstruction/ContactForm/ContactForm.js
+++ src/Reconstruction/ContactForm/ContactForm.js
@@ -47,11 +47,11 @@ const ContactForm = () => {
         <form className="email-form" onSubmit={sendEmail}>
           <h3>Email Me</h3>
           <label>Name</label>
-          <input type="text" name="user_name" />
+          <input type="text" name="user_name" required />
           <label>Email</label>
-          <input type="email" name="user_email" />
+          <input type="email" name="user_email" required />
           <label>Message</label>
-          <textarea name="message" />
+          <textarea name="message" required />
           <input
             id="submit-button"
             type="submit"
