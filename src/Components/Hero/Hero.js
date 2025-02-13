43b94632ad6ee84d95c8a3f28ae452f4aef00e9c
demaceo <hdemaceo@gmail.com>
2025-02-13 14:51:26 -0700

    add and adjust animations for Hero text

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,21 +1,23 @@
-import './Hero.css'
-import React, { useState } from 'react'
+import "./Hero.css";
+import React, { useState } from "react";
 
-export default function Hero () {
-  const [hovered, setHovered] = useState(false)
+export default function Hero() {
+  const [hovered, setHovered] = useState(false);
 
   return (
-    <section className='hero-container'>
-      <div
-        className={`hero-text-container ${hovered ? 'hovered' : ''}`}
-        onMouseEnter={() => setHovered(true)}
-      >
-        <h2 id='top-text'>Making the invisible</h2>
-        <h1 id='bttm-text'>VISIBLE</h1>
+    <section className="hero-container">
+      <div className={`hero-text-container`}>
+        <h2 id="top-text">Making the invisible</h2>
+        <div
+          className={`bttm-text-container ${hovered ? "hovered" : ""}`}
+          onMouseEnter={() => setHovered(true)}
+        >
+          <h1 id="bttm-text">VISIBLE</h1>
+        </div>
       </div>
-      <a href='#services-slide' className=''>
-        <div className='scroll-down'></div>
+      <a href="#services-slide" className="">
+        <div className="scroll-down"></div>
       </a>
     </section>
-  )
+  );
 }
