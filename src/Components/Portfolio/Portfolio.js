723963904779ba3c8ef31c203b98d3643129b24f
demaceo <hdemaceo@gmail.com>
2025-03-02 21:12:20 -0700

    conditional render project img or icon

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import projectData from "../../utilities/projectData.js";
 import "./Portfolio.css";
-
+import TinyDesk from "../TinyDesk/TinyDesk.tsx";
 export default function Portfolio() {
+  const [imageError, setImageError] = useState(false);
+
   const projects = projectData.map(
-    ({ id, image, name, description, partners, link }) => (
-      <section className="project-container" key={id} id={id}>
-        <a className="img-container" href={link}>
-          <img className="project-img" src={image} alt={name} />
-        </a>
-        <div className="project-details">
-          <h1 id="project-title">{name}</h1>
-          <p id="project-description">{description}</p>
-          <div className="contributors">
-            Contributors:
-            {partners.map((partner) => (
-              <a className="contributor" key={partner.link} href={partner.link}>
-                {partner.name}
-              </a>
-            ))}
+    ({ id, image, name, description, link, icon }) => {
+      return (
+        <section className="project-container" key={id} id={id}>
+          <a className="img-container" href={link}>
+            {!imageError && image ? (
+              <img
+                className="project-img"
+                src={image}
+                alt={name}
+                onError={() => setImageError(true)}
+              />
+            ) : (
+              <i className={`${icon} icon`}></i>
+            )}
+          </a>
+          <div className="project-details">
+            <h1 id="project-title">{name}</h1>
+            <p id="project-description">{description}</p>
           </div>
-        </div>
-      </section>
-    )
+        </section>
+      );
+    }
   );
 
-  return <section className="projects-container">{projects}</section>;
+  return <><section className="projects-container"><TinyDesk/>{projects}</section></>;
 }
