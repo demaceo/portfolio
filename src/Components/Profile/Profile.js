ba0fe0d6f16319da5e8f6f8ff371046d2fd0c973
demaceo <hdemaceo@gmail.com>
2025-05-22 12:47:18 -0600

    remove dead code

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,12 +1,10 @@
 import "./Profile.css";
 import ServiceSlide from "../ServiceSlide/ServiceSlide";
-import PrinciplesSlide from "../PrinciplesSlide/PrinciplesSlide";
 import AccordionPrinciples from "../PrinciplesSlide/AccordionPrinciples";
 export default function Profile() {
   return (
     <section className="profile-slides-container">
       <ServiceSlide />
-      {/* <PrinciplesSlide /> */}
       <AccordionPrinciples />
     </section>
   );
