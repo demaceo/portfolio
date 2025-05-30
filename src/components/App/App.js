3ffa5498581bc5b7d8b4d7ca2aa2ac35383e275f
demaceo <hdemaceo@gmail.com>
2025-05-30 10:07:54 -0600

    reinstall

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
  import { Routes, Route } from "react-router-dom";
 import NavBar from "../NavBar/NavBar.js";
 import Hero from "../Hero/Hero.js";
-import Profile from "../Profile/Profile.js";
-import Contact from "../Contact/Contact.js";
-import Projects from "../Projects/Projects.js";
+import Profile from "../Profile/Profile";
+import Contact from "../Contact/Contact";
+import Projects from "../Projects/Projects";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
