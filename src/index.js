5682bbfa65b71d86bbed99140b555bde4e483f33
demaceo <hdemaceo@gmail.com>
2025-07-17 12:53:19 -0600

    Fix import path to use correct case: Components vs components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./components/App/App.js";
+import App from "./Components/App/App.js";
 import reportWebVitals from "./reportWebVitals";
 import RouterProvider from "./RouterProvider";
 import { Analytics } from "@vercel/analytics/react";
